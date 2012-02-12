6c58dcb9729af599a9bd35185e699ef965d057ec
Michal Jedynak <m.jedynak@gmail.com>
2012-02-12 22:09:17 +0100

    added example of concurrent map built with guava

--- src/test/java/pl/mjedynak/concurrency/guava/ConcurrentMapExampleTest.java
+++ src/test/java/pl/mjedynak/concurrency/guava/ConcurrentMapExampleTest.java
@@ -2,7 +2,7 @@ package pl.mjedynak.concurrency.guava;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.ConcurrentMap;
@@ -13,31 +13,36 @@ import static org.junit.Assert.assertThat;
 
 public class ConcurrentMapExampleTest {
 
-    @Test
-    public void shouldAddToMap() {
-        // given
-        Cache<Integer, String> cache = CacheBuilder.newBuilder().build();
-        ConcurrentMap<Integer, String> concurrentMap = cache.asMap();
+    private static final int TIMEOUT_IN_MILLIS = 500;
+    private static final int KEY = 1;
+    private static final String VALUE = "David de Gea";
 
-        // when
-        concurrentMap.put(1, "David de Gea");
+    private ConcurrentMap<Integer, String> concurrentMap;
 
-        // then
-        assertThat(concurrentMap.size(), is(1));
+    @Before
+    public void setUp() {
+        Cache<Integer, String> cache = CacheBuilder.newBuilder().expireAfterWrite(TIMEOUT_IN_MILLIS, TimeUnit.MILLISECONDS).build();
+        concurrentMap = cache.asMap();
     }
-
-    @Ignore
+    
     @Test
     public void shouldRemoveElementWhenTimeoutExpires() throws InterruptedException {
-        // given
-        Cache<Integer, String> cache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).build();
-        ConcurrentMap<Integer, String> concurrentMap = cache.asMap();
-
         // when
-        concurrentMap.put(2, "Rafael da Silva");
-        Thread.sleep(2000);
+        concurrentMap.put(KEY, VALUE);
+        Thread.sleep(TIMEOUT_IN_MILLIS * 2);  // wait for the element to be removed
 
         // then
-        assertThat(concurrentMap.size(), is(0));
+        assertThat(concurrentMap.containsKey(KEY), is(false));
+        assertThat(concurrentMap.containsValue(VALUE), is(false));
+    }
+
+    @Test
+    public void shouldNotRemoveElementWhenTimeoutDoesNotExpire() throws InterruptedException {
+        // when
+        concurrentMap.put(KEY, VALUE);
+
+        // then
+        assertThat(concurrentMap.containsKey(KEY), is(true));
+        assertThat(concurrentMap.containsValue(VALUE), is(true));
     }
 }
