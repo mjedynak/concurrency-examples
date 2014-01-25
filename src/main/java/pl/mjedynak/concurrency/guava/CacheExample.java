654591e6bd386e56480c7039f86342189710119c
Michal Jedynak <m.jedynak@gmail.com>
2014-01-25 13:42:46 +0100

    added test

--- src/main/java/pl/mjedynak/concurrency/guava/CacheExample.java
+++ src/main/java/pl/mjedynak/concurrency/guava/CacheExample.java
@@ -10,6 +10,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -17,6 +23,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static pl.mjedynak.concurrency.guava.Service.SERVICE_PROCESSING_TIME;
+import static pl.mjedynak.concurrency.guava.Service.VALUE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CacheExample {
@@ -65,17 +72,45 @@ public class CacheExample {
         // then
         assertThat(cache.stats().evictionCount(), is(1L));
     }
+
+    @Test
+    public void shouldCopeWithManyThreads() throws Exception {
+        // given
+        int nThreads = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+        List<Future<String>> results = new ArrayList<>();
+
+
+        // when
+        for (int i = 0; i < nThreads * 10; i++) {
+            Future<String> future = executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return cache.get(KEY);
+                }
+            });
+            results.add(future);
+        }
+
+        // then
+        for (Future<String> result : results) {
+            assertThat(result.get(), is(VALUE));
+        }
+        verify(service, times(1)).getData(KEY);
+    }
 }
 
 
 class Service {
     public static final long SERVICE_PROCESSING_TIME = 300;
+    public static final String VALUE = "value";
+
     public String getData(String key) {
         System.out.println("accessing " + key);
         try {
             Thread.sleep(SERVICE_PROCESSING_TIME);
         } catch (InterruptedException e) {
         }
-        return "value";
+        return VALUE;
     }
 }
